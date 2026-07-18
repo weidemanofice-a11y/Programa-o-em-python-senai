d1faba4ba9168cda62932c35a03d739e7c99b5ca
weidemanofice-a11y <weidemanofice@gmail.com>
2026-07-17 22:00:51 -0300

    Add files via upload

---
+++ Aula-3/man.py
@@ -0,0 +1,42 @@
+# ctrl + f5
+# shift + menter
+
+
+print ('Hello Word') # boleanos
+
+
+
+# imprima algo no ambiente de teste
+
+
+# dados primitivos
+
+
+# print(type(10))
+# print(type("strings ==  textos"))
+# print(type(10 + 2)) # int inteiro
+# print(type(5.5 - 1.2)) # float real
+# print(type(False)) # boleanos
+
+
+
+
+
+print(10 +  2) # soma
+print(10 -  2) # subtração
+print(10 *  2) # multiplicação
+print(10.0 /  2) # divisão
+print(10 //  2) # divisão  c/ 2 barras
+print(10 % 2) # módulo 
+print(16 ** 2) # potencia elevado
+
+
+#saida sempre vai ser vai ser verdadeiro ou falso
+
+
+print(10 > 2) # maior ?
+print(10 < 2) # menor ?
+print(10 >= 2) # maior OU igual ?
+print(10 <= 2) # menor OU igual ?
+print(10 == 2) # é igual? 
+print(10 != 2) # é diferente?
---
+++ Aula-3/sistemas_notas.py
@@ -0,0 +1,30 @@
+
+#===========================================
+#CALCULO SIMPLES DE MEDIA ALUNO
+
+# note_1 = 5
+# nota_2 = 4
+# nota_3 = 6
+
+# media = (note_1 + nota_2 + nota_3) / 3
+# print (media)
+
+aprovado = media >= 7
+recuperacao = media >= 5
+reprovado = media <5
+
+# print('situacao do aluno: ')
+# print('aprovado: ')
+# print(aprovado)
+# print('recuperacao: ')
+# print(recuperacao)
+# print('reprovado')
+# print(reprovado)
+
+
+
+nota1 = 10
+nota2 = 5
+soma = nota1 + nota2
+media = soma / 2
+resultado = (media >=5 and print('aprovado')) or  (media <4 and print('reprovado')) or (media == 4 and print('recuperação'))
