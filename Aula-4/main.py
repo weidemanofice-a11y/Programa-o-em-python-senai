4c3275eaed687a932dfba3bf13e614ae379a81f4
weidemanofice-a11y <weidemanofice@gmail.com>
2026-07-21 21:58:27 -0300

    Add files via upload

---
+++ Aula-4/main.py
@@ -0,0 +1,63 @@
+
+
+
+
+# print ('Weideman')
+# #
+# n1  =  float(input('Nº'))
+# op =  input('Digite a operação: ')
+# n2  =  float(input('Nº'))
+# resultado =  (op == '+' and (n1 +  n2)) or (op == '-' and n1 - n2) or (op == '*' and n1 * n2) or (op == '/' and n1 /n2)
+
+
+# print(resultado)
+
+
+
+# idade  =  int(input('Idade: '))
+# carta_motorista = input('Possui carta: ')
+
+
+# resultado = (idade >= 18 and carta_motorista == 'sim' and ('Pode dirigir...')) or ('Não pode ...')
+
+
+# print(resultado)
+
+#=========================================================
+# 1 - Crie um programa para efetuar a leitura de um número inteiro e apresentar o resultado do quadrado deste número.
+# numero = int(input('digite numero: '))
+# resultado = (numero ** 2)
+
+# print(resultado)
+
+
+#=========================================================
+# 2 - Crie duas variáveis para armazenar seu primeiro nome e sobrenome. Em seguida, concatene-as para formar seu nome completo e exiba o resultado.
+# nome = input('Digite seu nome: ')
+# sobrenome = input('Digite seu sobrenome: ')
+
+# print(nome, sobrenome)
+
+#=========================================================
+# 3 - Peça ao usuário para digitar dois números inteiros e armazene-os em variáveis. Realize a concatenação desses números como strings e exiba o resultado.
+# numero1 = input('Digite numero 1: ')
+# numero2 = input('Digite numero 2: ')
+
+# print(numero1, numero2)
+
+#=========================================================
+# 4 - Crie uma variável para armazenar a palavra "Python". Em seguida, adicione um número inteiro ao final da palavra usando a concatenação e exiba o resultado.
+# nome = ('python')
+# numero = input('Digite numero: ')
+
+# print(nome, numero)
+
+#=========================================================
+#5 - Declare uma variável contendo uma frase. Em seguida, peça ao usuário para digitar uma palavra e concatene essa palavra no final da frase. Exiba o resultado. 
+
+# frase = ('Esta certo.? NÃO.! Mas também não esta errrado..!  ')
+# palavra = input('Digite uma apalavra: ')
+
+# print(frase, palavra)
+
+
