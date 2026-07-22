27305e1ff7de9f39344c8e4cf077291f60aa46bc
weidemanofice-a11y <weidemanofice@gmail.com>
2026-07-22 20:12:25 -0300

    Add files via upload

---
+++ Aula-5/main.py
@@ -0,0 +1,177 @@
+
+
+
+
+
+# # 1
+# numero =  int(input('numero inteiro: '))
+# quadrado =  numero ** 2
+# print(quadrado)
+
+
+# # 2
+
+
+# nome = 'Beatriz'
+# sobrenome = 'Alves'
+# print(nome +  ' ' + sobrenome)
+# print(nome, sobrenome)
+
+
+# # 3
+
+
+# n1  = int(input('numero inteiro:'))
+# n2  = int(input('numero inteiro:'))
+# print(str(n1), str(n2))
+
+
+# # 4
+
+
+# v = 'Python'
+# print(v,10)
+
+
+# # 5
+
+
+# frase = 'Bom dia'
+# nome  =  input('Nome: ')
+
+
+# print(frase, nome)
+
+#===================================================
+
+#        Maneiras de concatenar
+
+# #    usando o sinal     +
+# print('isso é um texto ' + ' ' + '10')
+# # print(10 + 10)
+
+
+
+# #     usando o sinal     ,
+# print('isso é um texto ',  '10')
+
+
+
+# #     usando o sinal     % 
+
+
+# print('isso é um texto  %d'%(10))
+
+
+
+# #     usando o      .format()
+
+
+# print('isso é um texto  {}'.format(10))
+
+
+
+# #    usando o       f-string
+
+
+# print(f'isso é um texto  {10} ')
+
+
+#         =======  Exercicios  ==========
+
+
+#  1 Imprima uma mensagem de boas-vindas na tela.
+
+# print('Hello Python')
+
+#===================================
+
+#  2 Declare uma variável booleana verdadeiro
+# com o valor True e imprima seu tipo
+
+# numero = 10
+
+# print(bool(numero and 10)) 
+
+#==================================
+
+# 3 Imprima o resultado da multiplicação de dois números decimais de sua escolha
+
+
+# numero1  = 2
+# numero2  = 3
+# resultado = (numero1 * numero2)
+
+# print(resultado)
+#========================================
+
+#  4 Imprima o resultado da divisão (/)de dois números inteiros 
+#  de sua escolha.
+
+
+# numero1  = 14
+# numero2  = 2
+# resultado = (numero1 / numero2)
+
+# print(resultado)
+
+# =======================================
+
+# 5 Imprima o resultado da subtração de dois números inteiros de sua escolha
+
+
+# numero1  = 10
+# numero2  = 5
+# resultado = (numero1 - numero2)
+
+# print(resultado)
+
+#========================================
+
+# 6 Imprima o resultado da divisão (//)inteira de dois números inteiros 
+# de sua escolha.
+
+# numero1  = 10
+# numero2  = 5
+# resultado = (numero1 // numero2)
+
+# print(resultado)
+#===========================================
+# 7 Imprima o resultado da multiplicação de 4 números 
+# decimais de sua escolha
+
+# numero1  = 2
+# numero2  = 2
+# numero3  = 3
+# numero4  = 2
+# resultado = ((numero1 * numero2) * (numero3 * numero4))
+
+# print(resultado)
+
+# ==========================================
+
+# 8 Declare uma variável numero e atribua um número inteiro. Em seguida, 
+# imprima o dobro desse número
+
+# numero1  = 3
+# resultado = (numero1 * 2)
+
+# print(resultado)
+
+#==========================================
+# 9 Crie um sistema de cadastro com as estruturas que vc já 
+# conhece(Use apenas input, print e variavel)
+# nome, email, idade, cidade, gradução, estado civil 
+
+nome = input('Digite seu nome: ')
+email = input('Digite seu e-mal: ')
+cidade = input('Digite sua cidade: ')
+graduacao = input('Digite sua graduacao: ')
+estado = input('Digite seu estadi civil: ')
+
+
+print(nome)
+print(email)
+print(cidade)
+print(graduacao)
+print(estado)
