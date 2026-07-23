3ed4a9c53685511067e57cb300a160bdc5ebf2f7
weidemanofice-a11y <weidemanofice@gmail.com>
2026-07-23 19:43:31 -0300

    Add files via upload

---
+++ Aula-6/main.py
@@ -0,0 +1,72 @@
+
+#Calcule o módulo ( % ) de 17 por 4.
+# valor = 17
+# porcentagem = 4
+
+# resultado = (valor * porcentagem)/100
+
+# print(resultado)
+
+#==========================
+
+#  Multiplique 9 por 3 e, em seguida, eleve o resultado ao quadrado.
+
+# resultado = (9 * 3)**2
+# print(resultado)
+
+# ==========================
+
+# Calcule a raiz quadrada de 81.
+
+
+# resultado = (81 / 9)
+# print(resultado)
+
+#=========================
+
+#   Adicione 20 a 3 vezes 4.
+
+# resultado = (20 + 3) / 4
+# print(resultado)
+
+#==========================
+
+#Multiplique 15 por 2 e, em seguida, subtraia 7.
+
+# resultado = (15 + 2) - 7
+# print(resultado)
+
+
+#==========================
+#Eleve 5 ao cubo.
+
+
+# resultado = 5**3
+# print(resultado)
+
+#==========================
+#Calcule a média de 17, 21 e 25.
+
+# resultado = (17 + 21 + 25) / 3
+# print(resultado)
+
+#==========================
+#Multiplique 11 por 2 e adicione 7.
+
+
+# resultado = (11 * 2) + 7
+# print(resultado)
+
+#==========================
+#Subtraia 15 de 3 vezes 8 e divida o resultado por 2.
+
+resultado = ((15 - 3) * 8) / 2
+print(resultado)
+
+
+
+#==========================
+#Eleve 2 à potência de 10.
+
+resultado = (10 ** 2) 
+print(resultado)
